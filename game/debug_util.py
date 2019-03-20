80ec1d08cce001f689141746af58b2736970a476
lreyn <lreyn@umich.edu>
2019-03-20 12:18:00 -0400

    reorganization

--- game/debug_util.py
+++
@@ -1,7 +0,0 @@
-def print_container(container):
-    print('{0} items in container'.format(len(container)))
-    for item in container:
-        print('id: {0}, name: {1}, aliases: [{2}], type: {3}'.format(item.id,
-                                                                     item.name,
-                                                                     ', '.join(item.aliases),
-                                                                     type(item)))
--- tests/house_test.py
+++ tests/house_test.py
@@ -1,62 +1,64 @@
-from room import RoomGame
-import catalogue
-import roomutil
-from debug_util import *
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import txtvrse
 
-game = RoomGame()
-game.load_lexicon(verb_file='wordmappings')
-foyer = game.create_room('foyer')
-game.alter_attributes(foyer, aliases=['hallway', 'entryway'])
-living_room = game.create_room(name='living_room')
-dining_room = game.create_room(name='dining_room')
-kitchen = game.create_room(name='kitchen')
-downstairs_bathroom = game.create_room('bathroom')
-mystery_room = game.create_room('mystery_room')
-basement = game.create_room('basement')
-game.alter_attributes(basement, aliases=['cellar'])
-upstairs_hallway = game.create_room('hallway')
-small_bedroom = game.create_room('bedroom')
-upstairs_bathroom = game.create_room('bathroom')
-medium_bedroom = game.create_room('bedroom')
-large_bedroom = game.create_room('bedroom')
-garage = game.create_room('garage')
-shoes = game.create_item('shoes',
+
+housegame = txtvrse.room.RoomGame()
+housegame.load_lexicon(verb_file='txtvrse/wordmappings')
+foyer = housegame.create_room('foyer')
+housegame.alter_attributes(foyer, aliases=['hallway', 'entryway'])
+living_room = housegame.create_room(name='living_room')
+dining_room = housegame.create_room(name='dining_room')
+kitchen = housegame.create_room(name='kitchen')
+downstairs_bathroom = housegame.create_room('bathroom')
+mystery_room = housegame.create_room('mystery_room')
+basement = housegame.create_room('basement')
+housegame.alter_attributes(basement, aliases=['cellar'])
+upstairs_hallway = housegame.create_room('hallway')
+small_bedroom = housegame.create_room('bedroom')
+upstairs_bathroom = housegame.create_room('bathroom')
+medium_bedroom = housegame.create_room('bedroom')
+large_bedroom = housegame.create_room('bedroom')
+garage = housegame.create_room('garage')
+shoes = housegame.create_item('shoes',
                          room=foyer,
                          article="",
                          portable=True)
-game.alter_attributes(shoes, aliases=['boots', 'footwear'], attributes=['stinky'])
+housegame.alter_attributes(shoes, aliases=['boots', 'footwear'], attributes=['stinky'])
 
-kitchen_foyer, foyer_kitchen = game.link_rooms(foyer, kitchen)
-living_foyer, foyer_living = game.link_rooms(foyer, living_room)
-dining_living, living_dining = game.link_rooms(living_room, dining_room)
-kitchen_dining, dining_kitchen = game.link_rooms(dining_room, kitchen)
-bath_kitchen, kitchen_bath = game.link_rooms(kitchen, downstairs_bathroom, door=True)
-kitchen_basement, basement_kitchen = game.link_rooms(basement, kitchen)
-kitchen_mystery, mystery_kitchen = game.link_rooms(mystery_room, kitchen)
-upstairs_foyer, foyer_upstairs = game.link_rooms(foyer, upstairs_hallway)
-game.alter_attributes(upstairs_foyer, aliases=['upstairs', 'stairs', 'up'])
-game.alter_attributes(foyer_upstairs, aliases=['downstairs', 'stairs', 'down'])
+kitchen_foyer, foyer_kitchen = housegame.link_rooms(foyer, kitchen)
+living_foyer, foyer_living = housegame.link_rooms(foyer, living_room)
+dining_living, living_dining = housegame.link_rooms(living_room, dining_room)
+kitchen_dining, dining_kitchen = housegame.link_rooms(dining_room, kitchen)
+bath_kitchen, kitchen_bath = housegame.link_rooms(kitchen, downstairs_bathroom, door=True)
+kitchen_basement, basement_kitchen = housegame.link_rooms(basement, kitchen)
+kitchen_mystery, mystery_kitchen = housegame.link_rooms(mystery_room, kitchen)
+upstairs_foyer, foyer_upstairs = housegame.link_rooms(foyer, upstairs_hallway)
+housegame.alter_attributes(upstairs_foyer, aliases=['upstairs', 'stairs', 'up'])
+housegame.alter_attributes(foyer_upstairs, aliases=['downstairs', 'stairs', 'down'])
 
-hallway_bathroom, bathroom_hallway = game.link_rooms(upstairs_bathroom, upstairs_hallway)
-hallway_sbr, sbr_hallway = game.link_rooms(small_bedroom, upstairs_hallway)
-hallway_mbr, mbr_hallway = game.link_rooms(medium_bedroom, upstairs_hallway)
-hallway_lbr, lbr_hallway = game.link_rooms(large_bedroom, upstairs_hallway)
+hallway_bathroom, bathroom_hallway = housegame.link_rooms(upstairs_bathroom, upstairs_hallway)
+hallway_sbr, sbr_hallway = housegame.link_rooms(small_bedroom, upstairs_hallway)
+hallway_mbr, mbr_hallway = housegame.link_rooms(medium_bedroom, upstairs_hallway)
+hallway_lbr, lbr_hallway = housegame.link_rooms(large_bedroom, upstairs_hallway)
 
-fridge = game.add_item(catalogue.Refrigerator(items_dict=game.items), room=kitchen)
-game.alter_attributes(fridge, attributes=['white', 'big'])
-cheese = game.create_item('cheese',
+fridge = housegame.add_item(txtvrse.catalogue.Refrigerator(items_dict=housegame.items), room=kitchen)
+housegame.alter_attributes(fridge, attributes=['white', 'big'])
+cheese = housegame.create_item('cheese',
                           room=kitchen,
                           location=fridge,
                           portable=True)
-counter = game.add_item(catalogue.Counter(items_dict=game.items), room=kitchen)
+counter = housegame.add_item(txtvrse.catalogue.Counter(items_dict=housegame.items), room=kitchen)
 
 
-go_upstairs = game.generate_action('go', upstairs_foyer)
-go_downstairs = game.generate_action('go', foyer_upstairs)
+go_upstairs = housegame.generate_action('go', upstairs_foyer)
+go_downstairs = housegame.generate_action('go', foyer_upstairs)
 
 go_upstairs.effects['change_location'][1] = lambda game: "You go upstairs."
 go_downstairs.effects['change_location'][1] = lambda game: "You go downstairs."
 
 
-game.init_game_state(foyer)
-game.run()
+housegame.init_game_state(foyer)
+housegame.run()
+
